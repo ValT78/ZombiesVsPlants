07d41bee92f1e8d9549a6896cde59f7305f2d114
AdriKat2022 <adrien.schroedel91@gmail.com>
2023-07-08 17:55:18 +0200

    Created plant manager prefab

--- Assets/Scripts/Plants/PlantManager.cs
+++ Assets/Scripts/Plants/PlantManager.cs
@@ -5,15 +5,43 @@ using UnityEngine;
 public class PlantManager : MonoBehaviour
 {
 
-    public GameObject[] plants;
+    public GameObject[] plantTypes; // All prefabs
+
+    private GameObject[,] plantMatrix = new GameObject[5, 12];
+
+    public ZombieManager zombieManager;
+
 
     void Start()
     {
-        // BasicPlantBehaviour.InstantiatePlant(plants[0], 0, 0);
+        for(int i = 0; i < 5; i++)
+		{
+            for(int j = 0; j < 12; j++)
+			{
+                InstantiatePlant(0, i, j);
+            }
+		}
     }
 
     void Update()
     {
         
+    }
+
+    public void InstantiatePlant(int plant, int linePos, int columnPos)
+    {
+        if(plantMatrix[linePos,columnPos] != null)
+		{
+            Debug.LogError("Can't spawn a plant on top of an existing plant !");
+            return;
+		}
+        GameObject instance = Instantiate(plantTypes[plant], new Vector3(columnPos, linePos, 0), Quaternion.identity);
+        instance.GetComponent<BasicPlantBehaviour>().Initialize(this, zombieManager, linePos, columnPos);
+        plantMatrix[linePos, columnPos] = instance;
+    }
+
+    public void FreePlantPlaceHolder(int linePos, int columnPos)
+	{
+        plantMatrix[linePos, columnPos] = null;
     }
 }
