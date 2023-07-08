0079f10a8dface25f886e8a812553eac3055992c
AdriKat2022 <adrien.schroedel91@gmail.com>
2023-07-09 01:45:46 +0200

    Floating brain in menu

---
+++ Assets/Scripts/UI/Animator.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animator : MonoBehaviour
+{
+
+	public GameObject icon, playButton, htpButton, creditsButton, quitButton;
+
+    private int hoveredOption;
+
+	private GameObject[] options;
+
+
+	void Start()
+	{
+		options = new GameObject[] { playButton, htpButton, creditsButton, quitButton };
+
+		MouserOver(0);
+	}
+
+
+	public void MouserOver(int hovered)
+	{
+		hoveredOption = hovered;
+		icon.transform.position = options[hoveredOption].transform.position - Vector3.right*130;
+	}
+
+}
