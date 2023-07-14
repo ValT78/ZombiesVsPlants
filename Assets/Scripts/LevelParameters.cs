71bf34f5ebe25a629cf40ccaa4741b1294b6b8c1
ValT <v.lantigny@gmail.com>
2023-07-14 14:18:05 +0200

    level menu

--- Assets/Scripts/LevelParameters.cs
+++ Assets/Scripts/LevelParameters.cs
@@ -13,9 +13,9 @@ public class LevelParameters : MonoBehaviour
 
     void Start()
     {
-        spawnBrains = new bool[] { true, true, true, true, true, true, true, true, false, true, true, true, false, true};
-        unlockedZombie = new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
-        sunMultiplier = new float[] { 0.5f, 0.5f, 0.55f, 0.6f, 0.65f, 0.7f, 0.75f, 0.8f, 0.85f, 0.9f, 0.95f, 1f, 1f, 1f };
+        spawnBrains = new bool[] { true, true, true, true, true, true, true, true, false, true, true, true, false, true, true};
+        unlockedZombie = new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+        sunMultiplier = new float[] { 0.5f, 0.5f, 0.55f, 0.6f, 0.65f, 0.7f, 0.75f, 0.8f, 0.85f, 0.9f, 0.95f, 1f, 1f, 1f, 1f};
         listLevel.Add(new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
         listLevel.Add(new int[] { 0, 3, 3, 2, 3, 3, 2, 3, 3, 2, 3, 3, 6, 6, 3, 6, 6, 2 });
         listLevel.Add(new int[] { 0, 3, 3, 6, 3, 3, 3, 3, 2, 2, 2, 0, 6, 6, 3, 6, 6, 2 });
